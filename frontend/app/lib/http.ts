24dc228141eba617ac193a5f03923b623a71f484
Samuel Monteiro <samuel.dvmonteiro@gmail.com>
2026-05-28 18:32:52 -0300

    ajuste4

--- frontend/app/lib/http.ts
+++ frontend/app/lib/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../hooks/store/use-auth';
 
 export const http = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10_000,
   headers: { 'Content-Type': 'application/json' },
 });
